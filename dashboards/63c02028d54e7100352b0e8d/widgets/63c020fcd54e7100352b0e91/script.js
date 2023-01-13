859f4cbacfaa845adc93f8da51f66a33dd612d9f
Tom Linton <thomas.linton%40sisense.com>
2023-01-13 16:16:38 +0000

    initial commit

---
+++ dashboards/63c02028d54e7100352b0e8d/widgets/63c020fcd54e7100352b0e91/script.js
@@ -0,0 +1,15 @@
+/*
+Welcome to your Widget's Script.
+
+To learn how you can access the Widget and Dashboard objects, see the online documentation at https://sisense.dev/guides/js/extensions
+*/
+
+prism.jumpToDashboard(
+	widget, 
+	{ 
+		displayDashboardsPane: false,
+		displayFilterPane: false,
+		displayHeaderRow: false,
+		displayToolbarRow: false,
+	},
+);
---
+++ dashboards/63c031f0d54e7100352b0eae/script.js
@@ -0,0 +1,16 @@
+/*
+Welcome to your Dashboard's Script.
+
+To learn how you can access the Widget and Dashboard objects, see the online documentation at https://sisense.dev/guides/js/extensions
+*/
+
+
+prism.jumpToDashboard(
+	widget, 
+	{ 
+		displayDashboardsPane: false,
+		displayFilterPane: false,
+		displayHeaderRow: false,
+		displayToolbarRow: false,
+	},
+);
